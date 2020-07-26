634487541c4853822a39fa36e10047a6ce6a1f24
Flipped <2946294893@qq.com>
2020-07-27 06:45:22 +0800

    wff

--- src/app/core/entity/entity.ts
+++ src/app/core/entity/entity.ts
@@ -69,6 +69,65 @@ export class Warning {
   company: Company;
 }
 
+export class Trouble {
+  id: number;
+  time: Date;
+  location: string;
+  num: WarnContent;
+  // tslint:disable-next-line:variable-name
+  machine_signal0: boolean;
+  // tslint:disable-next-line:variable-name
+  machine_signal1: boolean;
+  // tslint:disable-next-line:variable-name
+  machine_signal2: boolean;
+  // tslint:disable-next-line:variable-name
+  machine_signal3: boolean;
+  // tslint:disable-next-line:variable-name
+  machine_signal4: boolean;
+  // tslint:disable-next-line:variable-name
+  machine_signal5: boolean;
+  // tslint:disable-next-line:variable-name
+  machine_signal6: boolean;
+  // tslint:disable-next-line:variable-name
+  machine_signal7: boolean;
+  // tslint:disable-next-line:variable-name
+  machine_signal8: boolean;
+  // tslint:disable-next-line:variable-name
+  machine_signal9: boolean;
+  // tslint:disable-next-line:variable-name
+  machine_signal10: boolean;
+  // tslint:disable-next-line:variable-name
+  machine_signal11: boolean;
+  // tslint:disable-next-line:variable-name
+  machine_signal12: boolean;
+  // tslint:disable-next-line:variable-name
+  machine_signal13: boolean;
+  // tslint:disable-next-line:variable-name
+  machine_signal14: boolean;
+  // tslint:disable-next-line:variable-name
+  machine_signal15: boolean;
+  // tslint:disable-next-line:variable-name
+  machine_signal16: boolean;
+  // tslint:disable-next-line:variable-name
+  machine_signal17: boolean;
+  // tslint:disable-next-line:variable-name
+  machine_signal18: boolean;
+  // tslint:disable-next-line:variable-name
+  machine_signal19: boolean;
+  // tslint:disable-next-line:variable-name
+  machine_signal20: boolean;
+  // tslint:disable-next-line:variable-name
+  machine_signal21: boolean;
+  // tslint:disable-next-line:variable-name
+  machine_signal22: boolean;
+  // tslint:disable-next-line:variable-name
+  machine_signal23: boolean;
+  // tslint:disable-next-line:variable-name
+  machine_signal24: boolean;
+  robot: Robot;
+  company: Company;
+}
+
 export class BenchData {
   id: number;
   number: string;
@@ -352,5 +411,6 @@ export class Run {
   warn: number;
   time: Date;
   robot: Robot;
+  company: Company;
 }
 
--- src/app/rrp/yxqk/Service/yxsjtj.service.ts
+++ src/app/rrp/yxqk/Service/yxsjtj.service.ts
@@ -1,22 +1,41 @@
-import { Injectable } from '@angular/core';
-import {Response} from '../../../core/entity/entity';
+import {Injectable} from '@angular/core';
+import {Response, Trouble} from '../../../core/entity/entity';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {UrlService} from '../../../core/service/url.service';
 import {Observable, of} from 'rxjs';
 import {catchError} from 'rxjs/operators';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
 };
+
 @Injectable({
   providedIn: 'root'
 })
 export class YxsjtjService {
   response: Response;
   private runListUrl = this.url.hostname + '/run/getRun';
+  private runByCompanyListUrl = this.url.hostname + '/run/getRunByCompany';
   private runListByIdUrl = this.url.hostname + '/run/getRunById';
-  constructor(private http: HttpClient , private url: UrlService) { }
+  private troubleListUrl = this.url.hostname + '/trouble/getTrouble';
+  private troubleListByIdUrl = this.url.hostname + '/trouble/getTroubleById';
+  private troubleDeleteUrl = this.url.hostname + '/trouble/deleteById';
+  private troubleUpdateUrl = this.url.hostname + '/trouble/updateTrouble';
+  private troubleAddteUrl = this.url.hostname + '/trouble/addTrouble';
 
+  constructor(private http: HttpClient, private url: UrlService) {
+  }
+
+
+  /** GET runs from the server */
+  // tslint:disable-next-line:variable-name
+  getRunsByCompany(company_id: number, date_begin: string, date_end: string): Observable<Response> {
+    // tslint:disable-next-line:max-line-length
+    return this.http.get<Response>(this.runByCompanyListUrl + '?company_id=' + company_id + '&date_begin=' + date_begin + '&date_end=' + date_end)
+      .pipe(
+        catchError(this.handleError<Response>('getRunsByCompany'))
+      );
+  }
 
   /** GET runs from the server */
   // tslint:disable-next-line:variable-name
@@ -26,13 +45,47 @@ export class YxsjtjService {
         catchError(this.handleError<Response>('getRuns'))
       );
   }
+
   /** GET runs from the server */
   // tslint:disable-next-line:variable-name
   getRun(time: string): Observable<Response> {
     return this.http.get<Response>(this.runListByIdUrl + '?time=' + time)
       .pipe(
-        catchError(this.handleError<Response>('getRun'))
+        catchError(this.handleError<Response>('zgetRun'))
       );
+  }
+  /** DELETE: detail the trouble from the server */
+  deleteTrouble(trouble: Trouble | number): Observable<Response> {
+    const id = typeof trouble === 'number' ? trouble : trouble.id;
+    return this.http.post<Response>(this.troubleDeleteUrl, id, httpOptions).pipe(
+      catchError(this.handleError<Response>('deleteTrouble'))
+    );
+  }
+
+  /** GET troubles from the server */
+  getTroubles(): Observable<Response> {
+    return this.http.get<Response>(this.troubleListUrl)
+      .pipe(
+        catchError(this.handleError<Response>('getTroubles'))
+      );
+  }
+
+  /** GET trouble by id. Will 404 if id not found */
+  getTrouble(id: number): Observable<Response> {
+    const url = this.troubleListByIdUrl + '?id=' + id;
+    return this.http.get<Response>(url).pipe(
+      catchError(this.handleError<Response>(`getTrouble id=${id}`))
+    );
+  }
+  query(data) {
+    const url = this.url.hostname + '/trouble/QueryTrouble';
+    return new Promise(((resolve, reject) =>
+      this.http.post(url, data)
+        .toPromise().then(res => {
+        resolve(res);
+      }, error => {
+        reject(error);
+      })));
   }
 
   /**
